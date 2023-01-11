0c59d08e54a7f087698899b255c28e8ef8a8a0eb
loganbaby <shifo3456@gmail.com>
2023-01-11 23:57:00 +0300

    m3 added

---
+++ m3_2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Integer: MAX:" + Integer.MAX_VALUE + " MIN: " + Integer.MIN_VALUE);
+        System.out.println("Float: MAX:" + Float.MAX_VALUE + " MIN: " + Float.MIN_VALUE);
+        System.out.println("Double: MAX:" + Double.MAX_VALUE + " MIN: " + Double.MIN_VALUE);
+        System.out.println("Byte: MAX:" + Byte.MAX_VALUE + " MIN: " + Byte.MIN_VALUE);
+        System.out.println("Short: MAX:" + Short.MAX_VALUE + " MIN: " + Short.MIN_VALUE);
+        System.out.println("Long: MAX:" + Long.MAX_VALUE + " MIN: " + Long.MIN_VALUE);
+    }
+}
