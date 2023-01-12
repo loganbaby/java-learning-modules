6e43c888634986da2675d1e280a866e1ff1b684f
loganbaby <shifo3456@gmail.com>
2023-01-12 23:22:10 +0300

    added m3_6

--- m3_6/Main.java
+++ m3_6/Main.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    private static boolean filterIndexPart(final String[] parts, int index) {
+        return index < parts.length;
+    }
+
     public static void displayParseFIO(String fio) {
         String[] parts = fio.split(" ");
-        System.out.println("Name: " + parts[0]);
-        System.out.println("Surname: " + parts[1]);
-        System.out.println("Last name: " + parts[2]);
+
+        if(filterIndexPart(parts, 0)) {
+            System.out.println("Name: " + parts[0]);
+        } if(filterIndexPart(parts, 1)) {
+            System.out.println("Surname: " + parts[1]);
+        } if(filterIndexPart(parts, 2)) {
+            System.out.println("Last name: " + parts[2]);
+        }
     }
 
     public static void main(String[] args) {
