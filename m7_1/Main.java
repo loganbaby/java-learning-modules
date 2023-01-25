d4b5e22e8114bbeecad052dcf5c487c2267f68b8
loganbaby <shifo3456@gmail.com>
2023-01-25 22:53:04 +0300

    added m7_2

--- m7_1/Main.java
+++ m7_1/Main.java
@@ -1,17 +1,24 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws ParseException {
         ArrayList<Employee> staff = loadStaffFromFile();
         Collections.sort(staff, (Comparator.comparingInt(Employee::getSalary).thenComparing(Employee::getName)));
 
-        for (Employee employee : staff)
-            System.out.println(employee.toString());
+//        for (Employee employee : staff)
+//            System.out.println(employee.toString());
+
+        // m7_2 solution:
+        SimpleDateFormat year2017 = new SimpleDateFormat("yyyy");
+        Date year2017Date = year2017.parse("2018");
+        System.out.println(year2017Date.toString());
+
+        staff.stream().filter(e -> e.getWorkStart().after(year2017Date)).max(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
     }
 
     private static ArrayList<Employee> loadStaffFromFile()
